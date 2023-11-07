2cd3637a616131ac26230dce7580b13f91698b9b
saktiworkstation <saktiworkstation@gmail.com>
2023-11-07 17:33:10 +0700

    Memperbaiki halaman login

--- resources/views/auth/sigin.blade.php
+++ resources/views/auth/sigin.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
     <div class="self-container">
-        <h1>Login</h1>
+        <h1>SigIn</h1>
         <form id="loginForm" action="javascript:void(0);">
             <div class="self-form-group">
                 <label for="username">Username:</label>
@@ -13,7 +13,7 @@
                 <input type="password" id="password" name="password" required><br><br>
             </div>
 
-            <button type="submit">Login</button>
+            <button type="submit">SigIn</button>
         </form>
         <p>Belum memiliki akun? <a href="/register">Daftar disini</a></p>
     </div>
